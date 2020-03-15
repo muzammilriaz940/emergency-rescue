01a6f386f27b41b03030c62eda43aa14feb044b0
muzammilriaz940 <40453401+muzammilriaz940@users.noreply.github.com>
2020-03-15 19:25:27 +0500

    asdasd

--- app/src/main/java/com/example/emergencyrescue/Profile.java
+++ app/src/main/java/com/example/emergencyrescue/Profile.java
@@ -1,14 +1,20 @@
 package com.example.emergencyrescue;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
@@ -17,8 +23,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class Profile extends MainActivity implements
         View.OnClickListener {
@@ -32,6 +43,19 @@ public class Profile extends MainActivity implements
     private EditText profileUserService;
     private EditText profileBloodGroup;
     private String profilePassword;
+
+    // views for button
+    private Button btnSelect;
+
+    // Uri indicates, where the image will be picked from
+    private Uri filePath;
+
+    // request code
+    private final int PICK_IMAGE_REQUEST = 22;
+
+    // instance for firebase storage and StorageReference
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +71,22 @@ public class Profile extends MainActivity implements
 
         Intent intent = getIntent();
         profilePassword = intent.getStringExtra("profilePassword");
+
+        // initialise views
+        btnSelect = findViewById(R.id.btnChoose);
+
+        // get the Firebase storage reference
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        // on pressing btnSelect SelectImage() is called
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                SelectImage();
+            }
+        });
     }
 
     private boolean validateForm() {
@@ -95,6 +135,10 @@ public class Profile extends MainActivity implements
         mDatabase.child("Users").child(userId).child("mobile").setValue(mobile);
         mDatabase.child("Users").child(userId).child("userType").setValue(userType);
         mDatabase.child("Users").child(userId).child("bloodGroup").setValue(bloodGroup);
+        if(filePath != null){
+            uploadImage();
+            //mDatabase.child("Users").child(userId).child("image").setValue(filePath);
+        }
         if(userType.equals("user")) {
             mDatabase.child("Users").child(userId).child("service").setValue("");
         }else{
@@ -103,7 +147,7 @@ public class Profile extends MainActivity implements
         hideKeyboardFrom(Profile.this);
         View parentLayout = findViewById(R.id.profileRoot);
         Snackbar.make(parentLayout, "Profile Updated", Snackbar.LENGTH_LONG).show();
-        final Handler handler = new Handler();
+        /*final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -112,7 +156,7 @@ public class Profile extends MainActivity implements
                 startActivity(getIntent());
                 overridePendingTransition(0, 0);
             }
-        }, 3000);
+        }, 3000);*/
     }
 
     private void reAuthenticateUser(String userId, final String email, final String password) {
@@ -163,6 +207,133 @@ public class Profile extends MainActivity implements
                 reAuthenticateUser(user.getUid(), profileEmail.getText().toString(), profilePassword);
             }
             updateProfile(user.getUid(), profileName.getText().toString(), profileMobile.getText().toString(), profileUserType.getText().toString(), profileUserService.getText().toString(), profileBloodGroup.getText().toString());
+        }
+    }
+
+    // Select Image method
+    private void SelectImage()
+    {
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+    // Override onActivityResult method
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data)
+    {
+
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            /*try {
+
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(
+                                getContentResolver(),
+                                filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+
+            catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }*/
+        }
+    }
+
+    // UploadImage method
+    private void uploadImage()
+    {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            final ProgressDialog progressDialog
+                    = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            StorageReference ref
+                    = storageReference
+                    .child(
+                            "images/"
+                                    + UUID.randomUUID().toString());
+
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(
+                                        UploadTask.TaskSnapshot taskSnapshot)
+                                {
+
+                                    // Image uploaded successfully
+                                    // Dismiss dialog
+                                    progressDialog.dismiss();
+                                    View parentLayout = findViewById(R.id.profileRoot);
+                                    Snackbar.make(parentLayout, "Profile Updated", Snackbar.LENGTH_LONG).show();
+                                }
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+
+                            // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast
+                                    .makeText(Profile.this,
+                                            "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    .addOnProgressListener(
+                            new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                // Progress Listener for loading
+                                // percentage on the dialog box
+                                @Override
+                                public void onProgress(
+                                        UploadTask.TaskSnapshot taskSnapshot)
+                                {
+                                    double progress
+                                            = (100.0
+                                            * taskSnapshot.getBytesTransferred()
+                                            / taskSnapshot.getTotalByteCount());
+                                    progressDialog.setMessage(
+                                            "Uploaded "
+                                                    + (int)progress + "%");
+                                }
+                            });
         }
     }
 }
